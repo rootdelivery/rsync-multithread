e49553f5ae7780887f52dd1972145dc52bb9f954
Victor Byalskiy <byalskiyvo@eticket.kh.ua>
2023-12-25 13:22:30 +0200

    Threading logic v1

--- rsync_multithread.py
+++ rsync_multithread.py
@@ -1,90 +1,54 @@
 import subprocess
-import os
-import time
 import threading
+import os
+import re
 
-def generate_file_list(remote_source_directory, local_target_directory):
-    """
-    Generates a list of files to copy
-    """
-    if not remote_source_directory.endswith('/'):
-        remote_source_directory += '/'
+def execute_rsync(file_list, local_path):
+    for file in file_list:
+        rsync_command = f"rsync -avz {file} {local_path}"
+        subprocess.run(rsync_command, shell=True)
 
-    command = ["rsync", "-av", "--dry-run", "--itemize-changes", remote_source_directory, local_target_directory]
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err = process.communicate()
+def split_file_list(file_list_path, num_threads, remote_path):
+    with open(file_list_path, 'r') as file:
+        lines = file.readlines()
 
-    if process.returncode != 0:
-        raise RuntimeError(f"Rsync error: {err.decode().strip()}")
+    file_paths = []
+    for line in lines:
+        stripped_line = line.strip()
+        if stripped_line and not any(
+            phrase in stripped_line for phrase in ["receiving file list", "sent", "total size", "./"]
+        ):
+            file_paths.append(stripped_line)
 
-    # Filter filenames
-    return [line.split()[-1] for line in out.decode().split('\n') if line.startswith(">f")]
+    if not file_paths:
+        print("No files to transfer. Check the remote directory and rsync settings.")
+        return False
 
-def split_file_list(file_list, chunk_size=20):
-    """
-    Splits the file list into chunks.
-    """
-    for i in range(0, len(file_list), chunk_size):
-        yield file_list[i:i + chunk_size]
+    chunk_size = len(file_paths) // num_threads + (len(file_paths) % num_threads > 0)
+    for i in range(num_threads):
+        with open(f"thread_{i}_files.txt", 'w') as chunk_file:
+            chunk_files = [f"{remote_path}{file_path}\n" for file_path in file_paths[i*chunk_size:(i+1)*chunk_size]]
+            chunk_file.writelines(chunk_files)
 
-def write_chunks_to_files(chunks, chunk_dir):
-    """
-    Writes each chunk of files to a separate file in the chunk directory.
-    """
-    chunk_file_paths = []
-    for i, chunk in enumerate(chunks):
-        chunk_file_path = os.path.join(chunk_dir, f"chunk_{i}.txt")
-        with open(chunk_file_path, 'w') as f:
-            f.writelines('\n'.join(chunk))
-        chunk_file_paths.append(chunk_file_path)
-    return chunk_file_paths
+    return True
 
-def copy_files_threaded(chunk_file, remote_source_directory, local_target_directory):
-    """
-    Thread function to copy files listed in a chunk file using rsync.
-    """
-    try:
-        subprocess.run(["rsync", "-av", "--files-from=" + chunk_file, remote_source_directory, local_target_directory], check=True, stderr=subprocess.PIPE)
-    except subprocess.CalledProcessError as e:
-        print(f"Rsync process failed: {e.stderr.decode().strip()}")
-    finally:
-        if os.path.exists(chunk_file):
-            os.remove(chunk_file)
+def main(remote_path, local_path, dry_run_output, num_threads=10):
+    dry_run_command = f"rsync -avn {remote_path} {local_path} > {dry_run_output}"
+    subprocess.run(dry_run_command, shell=True)
 
-def main(remote_source_directory, local_target_directory, chunk_directory):
-    file_list = generate_file_list(remote_source_directory, local_target_directory)
-
-    if not os.path.exists(chunk_directory):
-        os.makedirs(chunk_directory)
-
-    chunk_files = write_chunks_to_files(split_file_list(file_list), chunk_directory)
+    if not split_file_list(dry_run_output, num_threads, remote_path):
+        return
 
     threads = []
-    for chunk_file in chunk_files:
-        thread = threading.Thread(target=copy_files_threaded, args=(chunk_file, remote_source_directory, local_target_directory))
+    for i in range(num_threads):
+        with open(f"thread_{i}_files.txt", 'r') as f:
+            file_list = [line.strip() for line in f.readlines() if line.strip()]
+        thread = threading.Thread(target=execute_rsync, args=(file_list, local_path))
         threads.append(thread)
         thread.start()
 
-        if len(threads) >= 5:  # Thread count
-            threads[0].join()  
-            threads.pop(0)  
-
-    # Wait for all threads to complete
     for thread in threads:
         thread.join()
 
-def is_rsync_running():
-    """
-    Checks if rsync is currently running.
-    """
-    try:
-        subprocess.run(["pgrep", "-x", "rsync"], check=True)
-        return True
-    except subprocess.CalledProcessError:
-        return False
-
 if __name__ == "__main__":
-    REMOTE_SOURCE_DIRECTORY = 'root@node1:/mnt/pve/HDD1/dump/'
-    LOCAL_TARGET_DIRECTORY = '/Users/levi/Documents/tmp/'  
-    CHUNK_DIRECTORY = '/Users/levi/Documents/chunk/'   
-    main(REMOTE_SOURCE_DIRECTORY, LOCAL_TARGET_DIRECTORY, CHUNK_DIRECTORY)
+    main('root@node1:/mnt/pve/HDD1/dump/', '/Users/levi/Documents/tmp/', '/Users/levi/Documents/chunk/output.txt')
